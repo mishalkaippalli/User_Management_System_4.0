e4cda35099e352517060bca8dcc256def34be0d1
Mishal <mishal9990@gmail.com>
2025-10-30 15:49:29 +0530

    json token authentication done

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,17 @@
-console.log('hello world')
+ const express = require('express')
+ const dotenv = require('dotenv').config()
+ const colors = require('colors')
+ const {errorHandler} = require('./middleware/errorMiddleware.js')
+ const connectDB = require('./config/db')
+ const port = process.env.PORT || 5000
+
+ connectDB()
+
+ const app = express()
+
+ app.use(express.json())
+ app.use(express.urlencoded({extended: false}))
+
+ app.use('/api/users', require('./routes/userRoutes'))
+
+ app.listen(port, () => console.log(`server running on port ${port}`))
